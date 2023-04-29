7b809f2ea80c72f5cd48e34153c5e009360baf2b
sukesan000 <oyama.k.1997@gmail.com>
2023-04-29 15:58:46 +0900

    .evvファイルを読み込めるように修正

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,5 +1,7 @@
 import { MicroCMSQueries, createClient } from "microcms-js-sdk";
 
+declare const require: any;
+require("dotenv").config();
 const client = createClient({
   serviceDomain: import.meta.env.SERVICE_DOMAIN,
   apiKey: import.meta.env.API_KEY,
